3e84f6993e513c329d99464099e382a53a5e0cb4
eaortiz <eaortiz@stanford.edu>
2014-07-19 18:00:34 -0700

    user form posting

--- routes/index.js
+++ routes/index.js
@@ -10,11 +10,12 @@ router.get('/user/create', function(req, res) {
 	res.render('create_user');
 });
 
-// router.post('/user/create', function(req, res) {
-// 	var name = req.body.name
-// 	var photo = req.body.photoUrl
-// 	var description = req.body.description
-// 	res.end('name' + name)
-// });
+router.post('/api/user/create', function(req, res) {
+	var name = req.body.name
+	var photo = req.body.photoUrl
+	var description = req.body.description
+	var contact = req.body.contact
+	res.end('name' + name)
+});
 
 module.exports = router;

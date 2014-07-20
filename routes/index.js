ffacc2ad6debb42b4aa8b1490bdd25dbb9557039
eaortiz <eaortiz@stanford.edu>
2014-07-19 17:37:15 -0700

    User creation form

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/user/create', function(req, res) {
+	res.render('create_user');
+});
+
+// router.post('/user/create', function(req, res) {
+// 	var name = req.body.name
+// 	var photo = req.body.photoUrl
+// 	var description = req.body.description
+// 	res.end('name' + name)
+// });
+
 module.exports = router;
